01208aad2861325045295e93858970116d7f38f1
Elton Mehmeti <97539294+EltonMehmeti@users.noreply.github.com>
2023-07-08 03:25:39 +0200

    Elton

---
+++ src/data.js
@@ -0,0 +1,52 @@
+export const Projects = [
+  
+  {
+    id: 1,
+   gitUN:"EltonMehmeti",
+   repoN: "MyPorfolio",
+  }, 
+  
+  {
+    id: 2,
+     gitUN:"EltonMehmeti",
+     repoN:"WordPressTheme",
+   } , 
+   {
+    id: 3,
+     gitUN:"EltonMehmeti",
+     repoN: "RecipeBook",
+   } , 
+   {
+    id: 4,
+     gitUN:"EltonMehmeti",
+     repoN: "BlogReact",
+   } , 
+   {
+    id: 5,
+     gitUN:"EltonMehmeti",
+     repoN: "DSHouses",
+   } , 
+   {
+    id: 6,
+     gitUN:"EltonMehmeti",
+     repoN: "CurrencyConverter",
+   } , 
+   {
+    id: 7,
+     gitUN:"EltonMehmeti",
+     repoN: "E-Commerce",
+   } , 
+   {
+    id: 8,
+   gitUN:"EltonMehmeti",
+   repoN: "Simple-NewsApi",
+ }, 
+ {
+  id: 8,
+   gitUN:"EltonMehmeti",
+   repoN: "elton-mehmeti-portfolio",
+ } ,
+
+ 
+
+]
